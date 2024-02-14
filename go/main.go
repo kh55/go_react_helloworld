acd3fb42d8630e383cc5eef7d8a75f9e27194c50
kazuhiko hyodo <k-hyodo@nissonet.co.jp>
2024-02-14 19:23:54 +0900

    API追加

--- go/main.go
+++ go/main.go
@@ -1,14 +1,42 @@
 package main
 
 import (
-	"log"
-
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
+	"time"
+	"log"
 )
 
 func main() {
 	log.Println("start server...")
 	r := gin.Default()
+	// ここからCorsの設定
+	r.Use(cors.New(cors.Config{
+		// アクセスを許可したいアクセス元
+		AllowOrigins: []string{
+			"http://localhost:3000",
+			"http://localhost:3001",
+		},
+		// アクセスを許可したいHTTPメソッド(以下の例だとPUTやDELETEはアクセスできません)
+		AllowMethods: []string{
+			"POST",
+			"GET",
+			"OPTIONS",
+		},
+		// 許可したいHTTPリクエストヘッダ
+		AllowHeaders: []string{
+			"Access-Control-Allow-Credentials",
+			"Access-Control-Allow-Headers",
+			"Content-Type",
+			"Content-Length",
+			"Accept-Encoding",
+			"Authorization",
+		},
+		// cookieなどの情報を必要とするかどうか
+		AllowCredentials: true,
+		// preflightリクエストの結果をキャッシュする時間
+		MaxAge: 24 * time.Hour,
+	  }))
 	r.GET("/hello", func(context *gin.Context) {
 		context.JSON(200, gin.H{
 			"message": "Hello World!",
